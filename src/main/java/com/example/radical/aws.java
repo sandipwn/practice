67e869371aca96e1119fa044c08101b9ad6ca82c
sandipwn <naikwadesv@gmail.com>
2023-06-11 21:37:23 +0530

    second commit

---
+++ src/main/java/com/example/radical/aws.java
@@ -0,0 +1,13 @@
+package com.example.radical;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class aws {
+    @GetMapping("/aws")
+    public String aws()
+    {
+        return "route53,ec2,lambda,efs";
+    }
+}
---
+++ src/main/java/com/example/radical/datascience.java
@@ -0,0 +1,13 @@
+package com.example.radical;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class datascience {
+    @GetMapping("/datascience")
+    public String dataScience()
+    {
+        return "learned datascience";
+    }
+}
