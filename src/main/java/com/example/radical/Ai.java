036b7777ddb86096df1aedf47667979e69f36b4c
Sandip Naikwade <57704655+sandipwn@users.noreply.github.com>
2023-06-18 21:33:08 +0530

    Update Ai.java

--- src/main/java/com/example/radical/Ai.java
+++ src/main/java/com/example/radical/Ai.java
@@ -8,7 +8,7 @@ public class Ai {
     @GetMapping("/Ai")
     public String dataScience()
     {
-        return "learned Ai now";
+        return "for conflict commited code from github";
     }
 }
 
