064f80482117f396b0c648f34440fcd58881a829
sandipwn <naikwadesv@gmail.com>
2023-07-01 23:05:07 +0530

    to check jenkins maven

--- src/main/java/com/example/radical/aws.java
+++ src/main/java/com/example/radical/aws.java
@@ -8,6 +8,6 @@ public class aws {
     @GetMapping("/aws")
     public String aws()
     {
-        return "route53,ec2,lambda,efs,new changes";
+        return "route53,ec2,lambda,efs,new changes1";
     }
 }
