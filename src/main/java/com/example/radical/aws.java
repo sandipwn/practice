ad64ef94102f3b4eff2a83114ed54052d122e5dc
sandipwn <naikwadesv@gmail.com>
2023-07-05 19:22:54 +0530

    Changes for cicd pipeline

--- src/main/java/com/example/radical/aws.java
+++ src/main/java/com/example/radical/aws.java
@@ -8,6 +8,6 @@ public class aws {
     @GetMapping("/aws")
     public String aws()
     {
-        return "route53,ec2,lambda,efs,new changes,new changes for cicd1";
+        return "route53,ec2,lambda,efs,new changes,new changes for cicd2";
     }
 }
