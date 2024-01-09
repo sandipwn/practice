f66df53c8ad125b0ab225c239aed074dd253cd18
unknown <naikwadesv@gmail.com>
2024-01-09 14:14:57 +0530

    new changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM tomcat:9.0.52-jre11-openjdk-slim
 COPY ./target/radical*.jar /usr/local/tomcat/webapps
-EXPOSE 8080
+EXPOSE 8080 80
+USER cowiapp
 WORKDIR /usr/local/tomcat/webapps
 CMD ["catalina.sh","run"]
--- src/main/java/com/example/radical/aws.java
+++ src/main/java/com/example/radical/aws.java
@@ -8,6 +8,6 @@ public class aws {
     @GetMapping("/aws")
     public String aws()
     {
-        return "route53,ec2,lambda,efs,new changes for maven,new changes for maven build trigger....";
+        return "route53,ec2,lambda,efs,new changes for maven,new changes for maven build trigger......";
     }
 }
